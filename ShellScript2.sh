f6ba9ae11ee48d233406f623bda8ba0ca6c27d05
CATARINA <catarina.carneiro@bandtec.com>
2020-11-29 22:29:04 -0300

    aaaaaa

--- ShellScript2.sh
+++ ShellScript2.sh
@@ -2,8 +2,14 @@
 which java | grep -q /bin/java
 if [ $? = 0 ]
 
-
 then echo "Realizando execução do Java"
+        if[[-d "$/home/a02201009/Desktop/JarIMC"]]
+then 
+        cd /home/a02201009/Desktop
+        git clone https://github.com/Catarina-Carneiro/JarIMC.git
+        java -jar Continua3-1.0-SNAPSHOT-jar-with-dependencies.jar 
+fi
+        cd /home/a02201009/Desktop
         java -jar Continua3-1.0-SNAPSHOT-jar-with-dependencies.jar 
 else echo "Realiazndo instalação do Java"
         sudo apt install curl
@@ -12,6 +18,7 @@ else echo "Realiazndo instalação do Java"
         source “$HOME/.sdkman/bin/sdkman-init.sh”
         sdk install java 8.0.265.j9-adpt
         echo "Realizando execução do Java"
-        cd /home/ubuntu/Desktop/JAVA/JarIMC/Continua3/target
+        cd /home/a02201009/Desktop
+        git clone https://github.com/Catarina-Carneiro/JarIMC.git
         java -jar Continua3-1.0-SNAPSHOT-jar-with-dependencies.jar 
 fi
