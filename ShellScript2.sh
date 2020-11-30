352c767c2a1f40d6a2f479620affdcaddeda54ae
CATARINA <catarina.carneiro@bandtec.com>
2020-11-29 23:13:47 -0300

    aaaaaaaaa

--- ShellScript2.sh
+++ ShellScript2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 which java | grep -q /bin/java
-if [ $? = 0 ]
-
-then echo "Realizando execução do Java"
-        if[[-d "$/home/a02201009/Desktop/JarIMC"]]
-                then echo "teste"
+if [[ $? = 0 ]]
+then  
+        echo "Realizando execução do Java"
+        if [[-d "$/home/a02201009/Desktop/JarIMC"]]
+        then 
+                echo "Esse diretorio existe"
         else         
                 cd /home/a02201009/Desktop
                 git clone https://github.com/Catarina-Carneiro/JarIMC.git
@@ -13,7 +14,8 @@ then echo "Realizando execução do Java"
         fi
         cd /home/a02201009/Desktop
         java -jar Continua3-1.0-SNAPSHOT-jar-with-dependencies.jar 
-else echo "Realiazndo instalação do Java"
+else    
+        echo "Realiazndo instalação do Java"
         sudo apt install curl
         sudo apt install curl
         curl -s “https://get.sdkman.io” | bash
